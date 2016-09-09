92cf37e345b5d57edc08fe778c13f5c9b34648ae
Fillps <filipess@live.com>
2016-09-09 00:05:42 -0300

    respeita os limites da RiotApi

--- src/data/ChallengerMatches.java
+++ src/data/ChallengerMatches.java
@@ -1,26 +1,28 @@
 package data;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.HashSet;
+import java.util.logging.Level;
 
 import constants.Patches;
 import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.league.dto.League;
+import net.rithms.riot.api.endpoints.league.dto.LeagueEntry;
+import net.rithms.riot.api.endpoints.match.dto.MatchDetail;
+import net.rithms.riot.api.endpoints.matchlist.dto.MatchList;
+import net.rithms.riot.api.endpoints.matchlist.dto.MatchReference;
 import net.rithms.riot.constant.QueueType;
 import net.rithms.riot.constant.Region;
-import net.rithms.riot.dto.League.League;
-import net.rithms.riot.dto.League.LeagueEntry;
-import net.rithms.riot.dto.Match.MatchDetail;
-import net.rithms.riot.dto.MatchList.MatchList;
-import net.rithms.riot.dto.MatchList.MatchReference;
+
 
 public class ChallengerMatches {
-	private RiotApi api;
-	private Region region;
-	private Patches patches;
+	private final RiotApi api;
+	private final Region region;
+	private final Patches patches;
 	
 	public ChallengerMatches(RiotApi api, Region region, Patches patches){
 		this.api = api;
@@ -28,20 +30,26 @@ public class ChallengerMatches {
 		this.patches = patches;
 	}
 	public List<MatchDetail> getChallengerMatches() throws RiotApiException, InterruptedException{
-		Set<Long> matchesIds = getChallengerMatchListIds();
-		Iterator<Long> it = matchesIds.iterator();
+		List<Long> matchesIds = new ArrayList<Long>(getChallengerMatchListIds());
+		System.out.println(matchesIds.size());
 		List<MatchDetail> listMatches = new ArrayList<MatchDetail>();
 		MatchDetail matchDetail = null;
-		int i = 1;
-		while (it.hasNext()){
-			try {
-				matchDetail = api.getMatch(region, it.next(), true);
-				listMatches.add(matchDetail);
-				System.out.println("Game " + i++);
-			} catch(RiotApiException e) {
-				e.printStackTrace();
-				Thread.currentThread().interrupt();
+		for (int i=0; i<matchesIds.size(); i++){
+			try {			
+				Thread.sleep(1200);
+			} catch (InterruptedException ex){
+				ex.printStackTrace();
+				throw new RuntimeException(ex);
 			}
+			try {
+				matchDetail = api.getMatch(region, matchesIds.get(i), true);
+				listMatches.add(matchDetail);
+				System.out.println("Game " + i);  
+			} catch(RiotApiException e) {
+				RiotApi.log.log(Level.SEVERE, "Waiting Interrupted", e);
+				if (e.getErrorCode()==429)
+					i--;
+			}  
 		}
 		return listMatches;
 	}
@@ -51,12 +59,21 @@ public class ChallengerMatches {
 		List<Long> matchesIds = new ArrayList<Long>();
 		MatchList matchList = null;
 		for(int i=0; i<ids.size();i++){
+			try {			
+				Thread.sleep(1200);
+			} catch (InterruptedException ex){
+				ex.printStackTrace();
+				throw new RuntimeException(ex);
+			}
 			try {
 				matchList = api.getMatchList(region, ids.get(i), "", "", "", patches.getBeginTime(), patches.getEndTime(), -1, -1);
-			} catch(RiotApiException e) {
-				e.printStackTrace();
-			    Thread.currentThread().interrupt();
-			}              
+				System.out.println(i);   
+			} catch(RiotApiException e) {   
+				RiotApi.log.log(Level.SEVERE, "Waiting Interrupted", e);
+				if (e.getErrorCode()==429)
+					i--;
+			}
+			
 			if (matchList!=null){
 				List<MatchReference> matchReferences = matchList.getMatches();
 				if (matchReferences!=null){
@@ -76,9 +93,10 @@ public class ChallengerMatches {
 		try {
 			challenger = api.getChallengerLeague(region, QueueType.RANKED_SOLO_5x5);
 		} catch(RiotApiException e) {
+			//RiotApi.log.log(Level.SEVERE, "Waiting Interrupted", e);
 			e.printStackTrace();
-		    Thread.currentThread().interrupt();
-		}
+			Thread.currentThread().interrupt();
+		}  
 		List<LeagueEntry> challengerEntry = challenger.getEntries();
     	Iterator<LeagueEntry> it = challengerEntry.iterator();
     	LeagueEntry entry;
--- src/teste/Teste1.java
+++ src/teste/Teste1.java
@@ -14,24 +14,33 @@ import java.io.ObjectOutputStream;
 
 import constants.Patches;
 import data.ChallengerMatches;
-import net.rithms.riot.constant.*;
-import net.rithms.riot.dto.Match.MatchDetail;
-import net.rithms.riot.api.*;
+import net.rithms.riot.api.ApiConfig;
+import net.rithms.riot.api.RiotApi;
+import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.match.dto.*;
+import net.rithms.riot.constant.Region;
+
+
+
 
 
 public class Teste1 {
 
 	public static void main(String[] args) throws RiotApiException, IOException, InterruptedException {
+		ApiConfig config = new ApiConfig().setKey(readFile("Key\\key.txt", Charset.defaultCharset()));
+		RiotApi api = new RiotApi(config);
 		
-		//RiotApi api = new RiotApi(readFile("Key\\key.txt", Charset.defaultCharset()));
-		
+		//Summoner a = api.getSummonerByName(Region.BR, "fillps");
+		//System.out.println(a.getName());
 		//MatchList matchList = api.getMatchList(Region.BR, id, "", "", "",Patches.V6_16.getBeginTime(), Patches.V6_16.getEndTime(), -1, -1);
-		//ChallengerMatches c = new ChallengerMatches(api,Region.EUW,Patches.V6_15);
-		//ChallengerMatches challengerMatches = new ChallengerMatches(api, Region.NA, Patches.V6_15);
-		//List<MatchDetail> list = challengerMatches.getChallengerMatches();
-		//escrever(list, "ChallegerMatches.NA.V6_15");
-		List<MatchDetail> list = ler("ChallegerMatches.NA.V6_15");
-		System.out.println(list.size());
+		//ChallengerMatches c = new ChallengerMatches(api,Region.BR,Patches.V6_15);
+		
+		ChallengerMatches challengerMatches = new ChallengerMatches(api, Region.BR, Patches.V6_15);
+		List<MatchDetail> list = challengerMatches.getChallengerMatches();
+		escrever(list, "ChallegerMatches.BR.V6_15");
+		
+		//List<MatchDetail> list = ler("ChallegerMatches.NA.V6_15");
+		//System.out.println(list.size());
 		
 	}
 	static String readFile(String path, Charset encoding) throws IOException {
