8f204ee0f9057bdb21dcc500398e35756dc5322c
Fillps <filipess@live.com>
2016-09-25 22:32:43 -0300

    comentarios em TestesDeDesempenho

--- src/TestesDeDesempenho.java
+++ src/TestesDeDesempenho.java
@@ -11,7 +11,36 @@ import net.rithms.riot.api.endpoints.match.dto.*;
 import sorting_algorithms.HeapSort;
 
 
-
+/*
+ * ETAPA II: TESTES DE DESEMPENHO (ATÉ 02/10/2016)
+ * 
+ * Desenvolvimento e teste de desempenho de algoritmos de classificação
+ * Preparar o subconjunto de dados, criando duas versões (ambas ordenadas de maneira decrescente): 
+ * uma ordenada pelo atributo numérico e outra ordenada pelo atributo categórico. Salve em arquivos 
+ * para poder carregar esses dados para depois não precisar refazer a ordenação.
+ * Implementar pelo menos os seguintes algoritmos:
+ * 		Insertion Sort com Busca Linear (ISBL)
+ *		Insertion Sort com Busca Binária (ISBB)
+ * 		Shell Sort (SHST)
+ * 		Bubble Sort (BBST)
+ * 		Quick Sort Randomizado (QSRM)
+ * 		Merge Sort (MGST)
+ * 		Heap Sort (HPST)
+ * 		Radix Sort MSD (RMSD) 
+ * Para todos os algoritmos, contabilizar o tempo de execução para pelo menos 3 tamanhos de array, tanto numérico quanto categórico:
+ * 100 elementos
+ * 1.000 elementos
+ * 10.000 elementos
+ * 100.000 elementos (opcional)
+ * 1.000.000 elementos (opcional)
+ *  
+ * Formato dos resultados:
+ * BBST, numerico, 1000, 10.
+ * BBST, numerico, 10000, 100.
+ * BBST, categorico, 1000, 70.
+ * ISBL, categorico, 1000, 1250.
+ *
+ */
 public class TestesDeDesempenho {
 	
 	public static void main(String[] args) throws IOException{
