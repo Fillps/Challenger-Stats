8bc31830a6d743aa0e7b5c975cfdcc31f86639d4
gabrijob <gabrijob@gmail.com>
2016-09-26 22:51:01 -0300

    implementação insertion sort com busca binária

--- src/sorting_algorithms/InsertionSortBuscaBinaria.java
+++ src/sorting_algorithms/InsertionSortBuscaBinaria.java
@@ -13,9 +13,31 @@ public class InsertionSortBuscaBinaria extends SortingAlgorithm {
 		sort(array,comparator);
 	}
 	
+	private static <T> int buscaBinaria(T[] array,T chave,int inf,int sup,Comparator<T> comparator){
+		int meio;
+		
+		while(inf<=sup){
+			meio=(inf+sup)/2;
+			if(comparator.compare(chave,array[meio])>0)
+				inf=meio+1;
+			else if(comparator.compare(chave,array[meio])<0)
+				sup=meio-1;
+			else
+				return meio;
+		}
+		return inf;	
+	}
+	
 	public static <T> void sort(T[] array, Comparator<T> comparator){
-		/*
-		 * TODO
-		 */
+		int pos;
+		T chave;
+		
+		for(int i=1; i<array.length; i++){
+			chave=array[i];
+			pos=buscaBinaria(array,chave,0,i-1,comparator);
+			for(int j=i; j>pos; j--)
+				array[j]=array[j-1];			
+			array[pos]=chave;
+		}	
 	}
 }
