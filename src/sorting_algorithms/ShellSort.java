c820fe6321014d2a41e86a4b895cf06698508072
MarcioLDN <marcio.narvaez@gmail.com>
2016-09-29 15:31:01 -0300

    Add files via upload

--- src/sorting_algorithms/ShellSort.java
+++ src/sorting_algorithms/ShellSort.java
@@ -8,14 +8,30 @@ public class ShellSort extends SortingAlgorithm {
 	public ShellSort(){
 		super(sigla);
 	}
-	
+
 	public <T> void sort_array(T[] array, Comparator<T> comparator){
 		sort(array,comparator);
 	}
-	
+
 	public static <T> void sort(T[] array, Comparator<T> comparator){
-		/*
-		 * TODO
-		 */
-	}
-}
+	    int h = 1;
+		int n = array.length;
+
+		while(h < n) h = h * 3 + 1;
+		h = h / 3;
+		int j;
+		T chave;
+
+		while (h > 0) {
+            for(int i = h; i < n; i++){
+                chave = array[i];
+                j = i;
+                while ((j >= h) && (comparator.compare(array[j-h], chave) > 0)) {
+                    swap(array, j, j-h);
+                    j = j - h;
+                }
+                array[j] = chave;
+            }
+            h = h / 2;
+		}
+     }
