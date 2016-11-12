f40dc9c60ea9036e1407a4bc0ce924b00676bc2e
Fillps <filipess@live.com>
2016-11-12 19:24:28 -0200

    caminho dos arquivos

---
+++ src/model/database/files/Path.java
@@ -0,0 +1,51 @@
+package model.database.files;
+
+
+/**
+ * Created by filip on 08/11/2016.
+ */
+public class Path {
+
+    public static final String MAIN_FOLDER = ("resources/");
+    public static final String IMAGE_FOLDER = ("images/");
+    public static final String DATA_FOLDER = ("data/");
+
+    public static final String MASTERY_ICONS = (MAIN_FOLDER + IMAGE_FOLDER + "mastery/");
+    public static final String MASTERY_BACK_1 = (MAIN_FOLDER + IMAGE_FOLDER + "mastery/masteryback_1.png");
+    public static final String MASTERY_BACK_2 = (MAIN_FOLDER + IMAGE_FOLDER + "mastery/masteryback_2.png");
+    public static final String CHAMPION_ICON = (MAIN_FOLDER + IMAGE_FOLDER + "champion/icon/");
+    public static final String CHAMPION_LOADING = (MAIN_FOLDER + IMAGE_FOLDER + "champion/loading/");
+    public static final String CHAMPION_SPLASH = (MAIN_FOLDER + IMAGE_FOLDER + "champion/splash/");
+    public static final String RUNE_ICONS = (MAIN_FOLDER + IMAGE_FOLDER + "rune/");
+    public static final String ITEM_ICONS = (MAIN_FOLDER + IMAGE_FOLDER + "item/");
+
+    public static final String BTREE_CHAMPION_RIOT_ID = (MAIN_FOLDER + DATA_FOLDER + "BTree_ChampionRiotID.bin");
+    public static final String BTREE_MASTERY_PAGE_RIOT_ID = (MAIN_FOLDER + DATA_FOLDER + "BTree_MasteryPageRiotID.bin");
+    public static final String BTREE_MASTERY_RIOT_ID = (MAIN_FOLDER + DATA_FOLDER + "BTree_MasteryRiotID.bin");
+    public static final String BTREE_ITEM_RIOT_ID = (MAIN_FOLDER + DATA_FOLDER + "BTree_ItemRiotID.bin");
+    public static final String BTREE_RUNE_RIOT_ID = (MAIN_FOLDER + DATA_FOLDER + "BTree_RuneRiotID.bin");
+    public static final String BTREE_SUMMONER_SPELL_RIOT_ID = (MAIN_FOLDER + DATA_FOLDER + "BTree_SummonerSpellRiotID.bin");
+    public static final String BTREE_RUNE_PAGE_RIOT_ID = (MAIN_FOLDER + DATA_FOLDER + "BTree_RunePageRiotID.bin");
+
+    public static final String BTREE_CHAMP_RUNE_PAGE = (MAIN_FOLDER + DATA_FOLDER + "BTree_Champion_RunePage.bin");
+    public static final String BTREE_CHAMP_SUMMONER_SPELL = (MAIN_FOLDER + DATA_FOLDER + "BTree_Champion_SummonerSpell.bin");
+    public static final String BTREE_CHAMP_MASTERY_PAGE = (MAIN_FOLDER + DATA_FOLDER + "BTree_Champion_MasteryPage.bin");
+    public static final String BTREE_CHAMP_ITEM = (MAIN_FOLDER + DATA_FOLDER + "BTree_Champion_Item.bin");
+
+    public static final String TRIE_CHAMPIONS_NAMES = (MAIN_FOLDER + DATA_FOLDER + "TrieMap_ChampionsNames.ser");
+    public static final String OVERALL_STATS = (MAIN_FOLDER + DATA_FOLDER + "OverallStats.ser");
+
+    public static final String CHAMPION_LIST = (MAIN_FOLDER + DATA_FOLDER + "ChampionList.ser");
+    public static final String ITEM_LIST = (MAIN_FOLDER + DATA_FOLDER + "ItemList.ser");
+    public static final String RUNE_LIST = (MAIN_FOLDER + DATA_FOLDER + "RuneList.ser");
+    public static final String RUNE_PAGE_LIST = (MAIN_FOLDER + DATA_FOLDER + "RunePageList.ser");
+    public static final String MASTERY_PAGE_LIST = (MAIN_FOLDER + DATA_FOLDER + "MasteryPageList.ser");
+    public static final String MASTERY_LIST = (MAIN_FOLDER + DATA_FOLDER + "MasteryList.ser");
+    public static final String SUMMONERSPEEL_LIST = (MAIN_FOLDER + DATA_FOLDER + "SummonerSpellList.ser");
+
+    public static final String CHAMPION_SUMMONERSPELL_LIST = (MAIN_FOLDER + DATA_FOLDER + "Champion_SummonerSpellList.ser");
+    public static final String CHAMPION_RUNE_PAEG_LIST = (MAIN_FOLDER + DATA_FOLDER + "Champion_RunePageList.ser");
+    public static final String CHAMPION_ITEM_LIST = (MAIN_FOLDER + DATA_FOLDER + "Champion_ItemList.ser");
+    public static final String CHAMPION_MASTERY_PAGE_LIST = (MAIN_FOLDER + DATA_FOLDER + "Champion_MasteryPageList.ser");
+
+}
