592d877bd8fed4cc3e4c66fb588615ada1be5cb5
gabrijob <gabrijob@gmail.com>
2016-11-13 16:41:08 -0200

    métodos binary files

--- src/model/database/files/BinaryFiles.java
+++ src/model/database/files/BinaryFiles.java
@@ -6,6 +6,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,17 +14,30 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import model.database.stats_structure.entity.Champion;
 
 public class BinaryFiles {
+	
+	
+	private static final String INPUT_FILE_NAME = "highscores.bin";
+	private static final String OUTPUT_FILE_NAME = "arqTeste.bin";
+	private static final String ITEM_FILE_NAME = "itens.bin";
+	private static final String CHAMPION_FILE = "champions.bin";
+	private static final String RUNE_FILE_NAME = "runes.bin";
+	private static final String MASTERY_FILE_NAME = "masteries.bin";
+	
+	private Map <Integer,Integer> mapChampion;
+	
 	
 	private static void log(Object aThing){
 	    System.out.println(String.valueOf(aThing));
 	}
 	
-	byte [] readAll(String aInputFilePath){ //aInputFilePath seria C:\\Users\\workspace\\nome.bin, vou mudar isso depois
-		log("Lendo em binario aquivo em: " + aInputFilePath);
-		File file = new File(aInputFilePath);
-		log("Tamanho do arquivo: " + file.length());
+	byte [] readAll(String aInputFileName){ //aInputFilePath seria C:\\Users\\workspace\\nome.bin, vou mudar isso depois
+		
+		log("Lendo em binario aquivo em: " + aInputFileName);
+		File file = new File(aInputFileName);
 		byte[] resultado = new byte[(int)file.length()];
 		
 		try{
@@ -54,9 +68,12 @@ public class BinaryFiles {
 		return resultado;
 	}
 	
-	byte [] readOnly(String aInputFilePath, int pos, int len ){
-		log("Lendo em binario aquivo em: " + aInputFilePath);
-		File file = new File(aInputFilePath);
+	byte [] readOnly(String aInputFileName, int pos, int len ){
+		log("Lendo em binario aquivo em: " + aInputFileName);
+		Path p = Paths.get(aInputFileName);		
+		
+		
+		File file = new File(aInputFileName);
 		log("Tamanho do arquivo: " + file.length());
 		byte[] resultado = new byte[(int)file.length()];
 		
@@ -64,7 +81,7 @@ public class BinaryFiles {
 			InputStream input = null;
 			try{
 				int BytesLidos = 0;
-				input = new BufferedInputStream(new FileInputStream(file));
+				input = new BufferedInputStream(Files.newInputStream(p));
 				BytesLidos = input.read(resultado, pos, len);					
 				if(BytesLidos > len) log("Erro no tamanho da entidade:" + pos);
 				log("Numero de bytes lidos: " + BytesLidos);		
@@ -95,4 +112,41 @@ public class BinaryFiles {
 		}
 	}
 
+	void writeAt(byte[] aInput, String aOutputFileName, int pos, int len){
+		File file = new File(aOutputFileName);
+		
+		try (FileOutputStream output = new FileOutputStream(file)){
+			output.write(aInput, pos, len);
+		}
+		catch(IOException ex){
+			log(ex);
+		}
+	}
+	
+	Object getObj(int id, String fileName, Map<Integer,Integer> dicionario) throws ClassNotFoundException, IOException{
+		int pos = dicionario.get(id);
+		
+		int len= dicionario.get(id+1)-pos; //Caga se n�o existir id+1, usar map.containsKey para a chave do final do arq
+		
+		byte[] data = readOnly(fileName, pos, len);
+		
+		return Serializer.deserialize(data);
+	}
+	
+	void saveChampion(Champion champion) throws IOException{
+		byte[] data = Serializer.serialize(champion);
+		int id=champion.getID();
+		//File file = new File(CHAMPION_FILE);
+		
+		if(mapChampion.containsKey(id))
+			writeAt(data, CHAMPION_FILE, mapChampion.get(id), mapChampion.get(-1));
+		else{
+			//if(!file.exists()) {mapChampion.put(-1,0);
+			//file.close();}
+			mapChampion.put(champion.getID(), mapChampion.get(-1));//mapChampion(key=-1) � a posicao do final do arquivo
+			write(data, CHAMPION_FILE);
+			int pos=mapChampion.get(-1);
+			mapChampion.put(-1,pos+data.length);
+		}
+	}
 }
